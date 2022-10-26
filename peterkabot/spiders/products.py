5a314e27bce65f755c19215b3f3ca31cdaa31df4
vo0doo <kirsanov.bvt@gmail.com>
2022-10-26 09:23:08 +0300

    update products.py

--- peterkabot/spiders/products.py
+++ peterkabot/spiders/products.py
@@ -10,7 +10,7 @@ class ProductsSpider(scrapy.Spider):
         self.url = 'https://products.5-delivery.ru/api/products/list'
         self.query = {
             "page": 1,
-            "stock": 3737,
+            "stock": 3737,  # 899
             "region": 1,
             "lpage": 200
         }
@@ -23,11 +23,11 @@ class ProductsSpider(scrapy.Spider):
 
         for product in products:
             yield {
-                "discount": product["sale_props"]["sale_percent"],
+                "discount": round(product["sale_props"]["sale_percent"]),
                 "name": product["name"],
                 "weight_volume_price": f"1 кг / {product['price']} руб" if product["by_weight"]["use"] else f"1 шт / {product['price']} руб",
                 "price": round(product["price"] * product["boxsize"], 2),
-                "old_price": product["sale"] * product["boxsize"] if product["sale"] > 0 else "Отсутствует",
+                "old_price": round(product["sale"] * product["boxsize"], 2) if product["sale"] > 0 else "Отсутствует",
                 "image_link": f"https://photos.5-delivery.ru/big{product['img']}",
             }
 
